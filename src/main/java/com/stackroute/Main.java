9502e9bdfc01e3025845f63745fb1c2fdc624f38
monieeesha <monikadri13@gmail.com>
2019-05-25 12:47:14 +0530

    File removal

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,22 +13,21 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
 
     @Deprecated
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("actorbean.xml");
-        Movie movie= (Movie)context.getBean("movie1");
-        System.out.println( movie);
+        Movie movie = (Movie) context.getBean("movie1");
+        System.out.println(movie);
 
         BeanFactory factory = new XmlBeanFactory(new ClassPathResource("actorbean.xml"));
-        Movie  movie1=(Movie)factory.getBean("movie2");
+        Movie movie1 = (Movie) factory.getBean("movie2");
         System.out.println(movie1);
 
         BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
         BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
         beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("actorbean.xml"));
-        Movie movie4=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie3");
-        System.out.println(movie4);
+        Movie movie3 = (Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie3");
+        System.out.println(movie3);
     }
 }
 
