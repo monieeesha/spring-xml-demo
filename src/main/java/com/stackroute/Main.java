208aa39d8f18d706bc920bb22f0e406b4f4028de
monieeesha <monikadri13@gmail.com>
2019-05-25 12:52:43 +0530

    File removal done

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,14 +15,17 @@ public class Main {
     @Deprecated
     public static void main(String[] args) {
 
+        //using application context
         ApplicationContext context = new ClassPathXmlApplicationContext("actorbean.xml");
         Movie movie = (Movie) context.getBean("movie1");
         System.out.println(movie);
 
+        //using BeanFactory
         BeanFactory factory = new XmlBeanFactory(new ClassPathResource("actorbean.xml"));
         Movie movie1 = (Movie) factory.getBean("movie2");
         System.out.println(movie1);
 
+        //using beanregisry and beandefinitionreader
         BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
         BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
         beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("actorbean.xml"));
